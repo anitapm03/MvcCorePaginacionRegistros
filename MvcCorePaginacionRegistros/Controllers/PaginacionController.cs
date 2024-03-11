1f1cf7419f83fe2ecca06aecce37e435c68632ae
anitapm03 <apereiramolinero@gmail.com>
2024-03-11 11:53:16 +0100

    Add project files.

---
+++ MvcCorePaginacionRegistros/Controllers/PaginacionController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCorePaginacionRegistros.Repositories;
+
+namespace MvcCorePaginacionRegistros.Controllers
+{
+    public class PaginacionController : Controller
+    {
+        private RepositoryHospital repo;
+
+        public PaginacionController(RepositoryHospital repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task<IActionResult>
+            PaginarRegistroVistaDepartamento(int? posicion)
+        {
+            if(posicion == null)
+            {
+                //ponemos la posicion en el primer registro
+                posicion = 1;
+            }
+            else
+            {
+
+            }
+            int numeroRegistros = await
+                this.repo.GetNumeroRegistrosVistaDepartamentos();
+
+            return View();
+        }
+    }
+}
---
+++ MvcCorePaginacionRegistros/Repositories/RepositoryHospital.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCorePaginacionRegistros.Data;
+using MvcCorePaginacionRegistros.Models;
+
+namespace MvcCorePaginacionRegistros.Repositories
+{
+    public class RepositoryHospital
+    {
+
+        private HospitalContext context;
+
+        public RepositoryHospital(HospitalContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<int> GetNumeroRegistrosVistaDepartamentos()
+        {
+            return await
+                this.context.VistaDepartamentos.CountAsync();
+        }
+
+        public async Task<VistaDepartamento>
+            GetVistaDepartamentoAsync(int posicion)
+        {
+            VistaDepartamento vista = await
+                this.context.VistaDepartamentos
+                .Where(z => z.Posicion == posicion)
+                .FirstOrDefaultAsync();
+            return vista;
+        }
+
+        public async Task<List<Departamento>> GetDepartamentosAsync()
+        {
+            return await this.context.Departamentos.ToListAsync();
+        }
+
+        public async Task<List<Empleado>> GetEmpleadosDepartamentoAsync
+            (int idDepartamento)
+        {
+            var empleados = this.context.Empleados
+                .Where(x => x.IdDepartamento == idDepartamento);
+            if (empleados.Count() == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return await empleados.ToListAsync();
+            }
+        }
+
+    }
+}
